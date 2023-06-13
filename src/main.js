4440f8bcb07df505bb7d797711a5b2d67d34de46
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-13 15:05:34 +0530

    b3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
   async function hello() {
-    console.log("Hello 2");
+    //console.log("Hello 2");
+    for(let i = 0 ;i<100 ;i++){
+      console.log(i * 2);
+    }
   }
   console.log("Hello 1");
   hello();
